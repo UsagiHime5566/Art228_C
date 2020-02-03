fdab113aeda1f3ec9d744464c9e6735c0619a5fe
nikki <cuapopomama@gmail.com>
2020-02-04 03:14:28 +0800

    Initial commit

---
+++ Assets/Scripts/StageManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class StageManager : MonoBehaviour
+{
+    public Button BTNView;
+    public Button BTNLeft;
+    public Button BTNRight;
+    public Button BTNHome;
+    
+    void ButtonView(){
+
+    }
+    void ButtonHome(){
+
+    }
+    void ButtonRight(){
+
+    }
+    void ButtonLeft(){
+
+    }
+}
