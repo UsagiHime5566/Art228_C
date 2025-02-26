91d4ff78a4c813bcff361a37630dd64f5ad70fe5
nikki <moniqshiin@gmail.com>
2025-02-26 15:12:04 +0800

    讓雙視窗同時顯示UI

---
+++ Assets/Scripts/MultiScreen.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MultiScreen : MonoBehaviour
+{
+    public Canvas MainCanvas;
+    public List<Camera> Cameras;
+    int loopIndex;
+
+    void Start()
+    {
+        foreach (var item in Display.displays)
+        {
+            item.Activate();
+        }
+    }
+
+    void Update()
+    {
+        MainCanvas.worldCamera = Cameras[loopIndex];
+        loopIndex = (loopIndex + 1) % Cameras.Count;
+    }
+}
